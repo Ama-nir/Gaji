75ac0a3d86666321122257c3f9d52f49270dc9e1
Ama-nir <SYAHRIALYT2@gmail.com>
2025-03-20 08:26:14 +0700

    Gaji Karyawan for

---
+++ Gajikaryawanfor.py
@@ -0,0 +1,44 @@
+nama = input("Masukkan Nama: ")
+nik = input("Masukkan NIK: ")
+
+status_list = ["Pegawai Tetap", "Honor"]
+golongan_list = ["A", "B", "C"]
+
+gaji_pokok = {
+    "Pegawai Tetap": 1000000,
+    "Honor": 750000
+}
+
+bonus_golongan = {
+    "Pegawai Tetap": {"A": 200000, "B": 400000, "C": 550000},
+    "Honor": {"A": 150000, "B": 275000, "C": 480000}
+}
+
+total_gaji_per_status = {
+    "Pegawai Tetap": 0,
+    "Honor": 0
+}
+
+print("\nDaftar Gaji dan Bonusnya")
+print("Nama:", nama)
+print("NIK:", nik)
+print("")
+
+for status in status_list:
+    for golongan in golongan_list:
+        gaji = gaji_pokok[status]
+        bonus = bonus_golongan[status][golongan]
+        gaji_total = gaji + bonus
+
+        total_gaji_per_status[status] += gaji_total
+
+        print("Status    :", status)
+        print("Golongan  :", golongan)
+        print("Gaji Pokok: Rp", gaji)
+        print("Bonus     : Rp", bonus)
+        print("Gaji Total: Rp", gaji_total)
+        print("")
+
+print("Total Gaji + Bonus untuk setiap Status:")
+for status in total_gaji_per_status:
+    print(status, ": Rp", total_gaji_per_status[status])
